50f3602dd5c285607d5890355c64dd8b4085ea5d
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-11-02 17:23:27 +0530

    api

--- libs/student-list/src/index.ts
+++ libs/student-list/src/index.ts
@@ -1,2 +1,2 @@
 export * from './lib/student-list/feature/student-list.component';
-export * from './lib/student-list/api/student-list.service';
+export * from './lib/student-list/data-access/api/student-list.service';
