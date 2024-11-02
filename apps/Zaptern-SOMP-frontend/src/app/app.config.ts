8d5cc4133ac6254cd07a07971e4781c69ecc9d63
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-11-02 14:24:32 +0530

    install effects

--- apps/Zaptern-SOMP-frontend/src/app/app.config.ts
+++ apps/Zaptern-SOMP-frontend/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideState('', {}),
+    provideEffects([]),
   ],
 };
