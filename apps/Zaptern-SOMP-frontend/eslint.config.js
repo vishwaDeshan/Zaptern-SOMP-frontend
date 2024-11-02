592826dd273098f81a333da017e24c516c8152da
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-11-02 11:43:16 +0530

    basic configs

--- apps/Zaptern-SOMP-frontend/eslint.config.js
+++ apps/Zaptern-SOMP-frontend/eslint.config.js
@@ -12,7 +12,7 @@ module.exports = [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: 'somp',
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ module.exports = [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: ['somp', 'app'],
           style: 'kebab-case',
         },
       ],
