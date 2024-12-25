14fbbbb590eb31896938c27f3ef9abe6b8916f1f
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2024-12-25 07:50:08 +0530

    add featureSelector

--- libs/student-list/src/lib/student-list/data-access/+state/student-list.selectors.ts
+++ libs/student-list/src/lib/student-list/data-access/+state/student-list.selectors.ts
@@ -1,5 +1,16 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
 import { StudentListState } from './student-list.reducers';
 
-export const selectStudentList = (state: StudentListState) => state;
+export const selectNavbarState =
+  createFeatureSelector<StudentListState>('studentList');
 
-export const selectError = (state: StudentListState) => state.error;
+export const selectStudentList = createSelector(
+  selectNavbarState,
+  (state: StudentListState) => state
+);
+
+export const selectError = createSelector(
+  selectNavbarState,
+  (state: StudentListState) => state.error
+);
