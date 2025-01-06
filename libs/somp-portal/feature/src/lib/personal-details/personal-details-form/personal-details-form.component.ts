3625331765a0f220042f9627fc07c5e4180e6985
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-06 12:59:33 +0530

    personal information page changes

--- libs/somp-portal/feature/src/lib/personal-details/personal-details-form/personal-details-form.component.ts
+++ libs/somp-portal/feature/src/lib/personal-details/personal-details-form/personal-details-form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -7,28 +7,68 @@ import {
   Validators,
 } from '@angular/forms';
 import { FormContainerComponent } from '@zaptern-somp-frontend/form-container';
-import { RestrictSpecialCharactersDirective } from '@zaptern-somp-frontend/directives';
+import {
+  ErrorMessageProcessor,
+  FormValidator,
+} from '@zaptern-somp-frontend/services';
+import { onlyAlphabeticCharsRegex } from '@zaptern-somp-frontend/helpers';
 
 @Component({
   selector: 'somp-personal-details-form',
   standalone: true,
   templateUrl: './personal-details-form.component.html',
   styleUrls: ['./personal-details-form.component.scss'],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormContainerComponent,
-    RestrictSpecialCharactersDirective,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, FormContainerComponent],
 })
-export class PersonalDetailsFormComponent {
-  personalDetailsForm: FormGroup;
+export class PersonalDetailsFormComponent implements OnInit {
+  personalDetailsForm: FormGroup | undefined;
+  errorMsgs: { [key: string]: string } = {};
+  private validationMessages: { [key: string]: { [key: string]: string } };
+  private formErrorMsgProcessor: ErrorMessageProcessor;
 
   constructor(private fb: FormBuilder) {
+    this.validationMessages = {
+      firstName: {
+        required: 'First name is required.',
+        maxlength: 'First name cannot exceed 50 characters.',
+        pattern: 'Invalid characters in first name.',
+      },
+      lastName: {
+        required: 'Last name is required.',
+        maxlength: 'Last name cannot exceed 50 characters.',
+        pattern: 'Invalid characters in last name.',
+      },
+    };
+
+    this.formErrorMsgProcessor = FormValidator.intValidator(
+      this.validationMessages
+    );
+  }
+
+  ngOnInit(): void {
     this.personalDetailsForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.maxLength(50)]],
-      middleName: ['', [Validators.maxLength(50)]],
-      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.pattern(onlyAlphabeticCharsRegex),
+        ],
+      ],
+      lastName: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.pattern(onlyAlphabeticCharsRegex),
+        ],
+      ],
+    });
+
+    this.personalDetailsForm?.valueChanges.subscribe(() => {
+      this.errorMsgs = this.formErrorMsgProcessor.processMessages(
+        this.personalDetailsForm!
+      );
     });
   }
 }
