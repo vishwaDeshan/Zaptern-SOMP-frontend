b25e33db9f1e2a050cefc8e314ab6a569db4d897
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-06 17:32:07 +0530

    auto savable form modifications

--- libs/shared/auto-savable-form/src/lib/auto-savable/auto-savable-form.component.ts
+++ libs/shared/auto-savable-form/src/lib/auto-savable/auto-savable-form.component.ts
@@ -1,5 +1,4 @@
 import {
-  AfterContentChecked,
   AfterContentInit,
   Component,
   ContentChild,
@@ -12,7 +11,6 @@ import {
   debounceTime,
   distinctUntilChanged,
   filter,
-  map,
   takeUntil,
 } from 'rxjs/operators';
 import { AutoSavableForm } from './auto-savable-form.interface';
@@ -20,98 +18,49 @@ import { AUTOSAVABLEFORM } from './auto-savable-form.token';
 
 @Component({
   selector: 'somp-auto-savable-form',
-  template: `<div (cdkObserveContent)="onContentChange()">
+  template: `<div>
     <ng-content></ng-content>
   </div>`,
 })
-export class AutoSavableFormComponent
-  implements AfterContentInit, OnDestroy, AfterContentChecked
-{
+export class AutoSavableFormComponent implements AfterContentInit, OnDestroy {
   @ContentChild(AUTOSAVABLEFORM) autoSavableForm!: AutoSavableForm;
   private autoSaveUnsubscribe = new Subject<void>();
   form!: FormGroup;
-  onFormLoadValidationsTriggered = false;
 
   constructor(private router: Router) {}
 
   ngAfterContentInit(): void {
-    // Access the form and subscribe to value changes
+    // Access form and subscribe to value changes
     this.form = this.autoSavableForm.form;
+
     this.form.valueChanges
-      .pipe(takeUntil(this.autoSaveUnsubscribe), distinctUntilChanged())
-      .subscribe((formData) => {
-        this.autoSavableForm.formStatus.emit({
-          isFormValid: this.form.valid,
-          isFormDirty: this.form.dirty,
-        });
-        this.autoSavableForm.formValuesChange.emit(formData);
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(500), // Adjust debounce as needed
+        takeUntil(this.autoSaveUnsubscribe)
+      )
+      .subscribe(() => {
+        // Auto-save if form is dirty and valid
+        if (this.form.valid && this.form.dirty) {
+          this.autoSavableForm.autoSave.emit(this.form.value);
+        }
       });
 
-    this.triggerAutoSave();
-
+    // Trigger validation checks when navigating away
     this.router.events
       .pipe(
         filter((event) => event instanceof NavigationStart),
         takeUntil(this.autoSaveUnsubscribe)
       )
       .subscribe(() => {
-        if (this.form.invalid && (this.form.dirty || this.form.touched)) {
+        if (this.form.dirty || this.form.touched) {
           this.triggerValidations();
         }
       });
   }
 
-  // Detect content change for validation trigger
-  onContentChange() {
-    if (!this.onFormLoadValidationsTriggered) {
-      this.triggerValidationOnFormLoad();
-    }
-  }
-
-  // Trigger form validation if required
-  private triggerValidationOnFormLoad() {
-    const { visitedRoutes, formData } = this.autoSavableForm;
-    if (
-      visitedRoutes?.length &&
-      visitedRoutes?.includes(this.router.url) &&
-      ((Array.isArray(formData) && formData.length > 0) ||
-        (!Array.isArray(formData) && formData))
-    ) {
-      this.triggerValidations();
-      this.onFormLoadValidationsTriggered = true;
-    }
-  }
-
-  // Mark form as touched and trigger validations
-  private triggerValidations(): void {
+  private triggerValidations() {
     this.form.markAllAsTouched();
-    this.autoSavableForm.validate();
-    this.autoSavableForm.formStatus.emit({
-      isFormValid: this.form.valid,
-      isFormDirty: this.form.dirty,
-    });
-  }
-
-  // Trigger auto-save functionality on form value change
-  private triggerAutoSave(): void {
-    this.form.valueChanges
-      .pipe(
-        distinctUntilChanged(),
-        debounceTime(500),
-        map((formValue: unknown) => {
-          if (this.form.valid && this.form.dirty) {
-            this.autoSavableForm.autoSave.emit(formValue);
-          }
-        }),
-        takeUntil(this.autoSaveUnsubscribe)
-      )
-      .subscribe();
-  }
-
-  ngAfterContentChecked(): void {
-    if (this.onFormLoadValidationsTriggered) {
-      this.form.markAllAsTouched();
-    }
   }
 
   ngOnDestroy(): void {
