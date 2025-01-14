00294614bcb224f88e3a6b671fe565f00f485751
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-14 08:30:28 +0530

    move top tool bar to a lib

--- apps/Zaptern-SOMP-frontend/src/app/app.component.ts
+++ apps/Zaptern-SOMP-frontend/src/app/app.component.ts
@@ -10,12 +10,18 @@ import {
   selectFormSavedSuccess,
   selectFormSaving,
 } from '@zaptern-somp-frontend/shared-data-access';
-import { ToolTopBarComponent } from 'libs/shared/components/top-tool-bar/top-tool-bar.component';
+import { ToolTopBarComponent } from '@zaptern-somp-frontend/components';
 import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, ToolTopBarComponent, SideBarComponent,NotificationsComponent, CommonModule],
+  imports: [
+    RouterModule,
+    ToolTopBarComponent,
+    SideBarComponent,
+    NotificationsComponent,
+    CommonModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- libs/shared/components/src/index.ts
+++ libs/shared/components/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/loading-popup/loading-popup.component';
 export * from './lib/date-picker/date-picker.component';
 export * from './lib/side-bar/side-bar.component';
 export * from './lib/notifications/notifications.component';
+export * from './lib/top-tool-bar/top-tool-bar.component';
