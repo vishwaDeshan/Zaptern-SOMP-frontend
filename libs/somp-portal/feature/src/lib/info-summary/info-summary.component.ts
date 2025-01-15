5b84493b169821a8a9d798cf412d1c4816e1b8a0
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-15 10:01:12 +0530

    remove unwanted imports

--- libs/somp-portal/feature/src/lib/info-summary/info-summary.component.ts
+++ libs/somp-portal/feature/src/lib/info-summary/info-summary.component.ts
@@ -1,19 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { InfoSummaryFormComponent } from './info-summary-form/info-summary-form.component';
-import { select, Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import {
   setPageTitle,
   showBorderLine,
   showSideBar,
   showTopToolBar,
 } from '@zaptern-somp-frontend/shared-data-access';
-import { Observable } from 'rxjs';
-import { PersonalDetails } from '@zaptern-somp-frontend/model';
-import {
-  loadPersonalDetails,
-  selectPersonalDetails,
-} from '@zaptern-somp-frontend/data-access';
 
 @Component({
   selector: 'somp-info-summary',
