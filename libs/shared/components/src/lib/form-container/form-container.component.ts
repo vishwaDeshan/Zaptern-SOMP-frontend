50664f3a994eaace5454b4eb7cd8dd42448f51d5
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-16 20:31:49 +0530

    form container changes

--- libs/shared/components/src/lib/form-container/form-container.component.ts
+++ libs/shared/components/src/lib/form-container/form-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './form-container.component.html',
   styleUrl: './form-container.component.scss',
 })
-export class FormContainerComponent {}
+export class FormContainerComponent {
+  @Input() showRequiredInput: boolean = true;
+}
