39cef46ca37f829496e9c6dd64f6de6d5c189058
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-01-22 12:09:12 +0530

    modify lib name

--- libs/shared/components/src/lib/timeLine/timeLine.component.ts
+++ libs/shared/components/src/lib/timeLine/timeLine.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { timelineData } from '@zaptern-somp-frontend/models';
+import { timelineData } from '@zaptern-somp-frontend/common/models';
 
 @Component({
   selector: 'somp-time-line',
